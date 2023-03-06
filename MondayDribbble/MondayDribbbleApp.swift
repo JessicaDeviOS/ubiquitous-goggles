60b37dd71e2c0326cad55647b211025b17d63596
Jessica Mallian <jlmallian@gmail.com>
2023-03-06 16:25:22 -0600

    created project

---
+++ MondayDribbble/MondayDribbbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MondayDribbbleApp.swift
+//  MondayDribbble
+//
+//  Created by Jessica Mallian on 3/6/23.
+//
+
+import SwiftUI
+
+@main
+struct MondayDribbbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
